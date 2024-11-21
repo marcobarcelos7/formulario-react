26a67f6ae903125d871dd2d94cf69dfb777c4179
Marco A Barcelos <marco.antonio_barcelos@hotmail.com>
2024-11-21 17:17:01 -0300

    formulario react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+
+  const enviarAlert = (event) => {
+    event.preventDefault()
+    console.log('Dados enviados');
+    console.log('Nome:', nome);
+    console.log('Email:', email)
+    alert(`Formulario enviado com sucesso ${nome} ${email}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='Form' onSubmit={enviarAlert}>
+          <h1 className='Title'>Formulário</h1>
+          <label className='Name'>
+            Nome: 
+
+            <input className='NameInput' type='text' value={nome} onChange={(event) => setNome(event.target.value)}></input>
+          </label>
+          <br />
+          <label className='Email'>
+            Email: 
+
+            <input className='EmailInput' type='email' value={email} onChange={(event) => setEmail(event.target.value)}></input>
+          </label>
+          <br />
+          <button className='SendButton' type='submit'>Enviar</button>
+      </form>
     </div>
   );
 }
